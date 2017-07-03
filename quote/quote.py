8cd5ae66ef903dba8b7509b359554d48f7f095fc
David Bidner <david.bidner@student.tugraz.at>
2017-07-03 21:15:37 +0200

    ADD: import random

--- quote/quote.py
+++ quote/quote.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 from cogs.utils.dataIO import dataIO
 from cogs.utils import checks
 import json
+import random
 
 class Quote:
     """Simple quote cog"""
@@ -18,7 +19,7 @@ class Quote:
         try:
             with open('quotes.json', 'r') as f:
                 self.quotes = json.load(f)
-        except:
+        except Exception:
             self.quotes = []
 
     async def on_reaction_add(self, reaction, user):
