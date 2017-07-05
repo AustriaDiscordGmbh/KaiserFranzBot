1ae09688d890f5552d0c6803008345bfab20e594
David Bidner <david.bidner@student.tugraz.at>
2017-07-05 13:29:49 +0200

    FIX: no empty suggestions

--- suggestionbox/suggestionbox.py
+++ suggestionbox/suggestionbox.py
@@ -105,6 +105,8 @@ class SuggestionBox:
     async def send_suggest(self, message, server):
         author = message.author
         suggestion = message.clean_content.replace("!suggest ", "", 1)
+        if(suggestion == "!suggest" or not suggestion)
+            return await self.bot.say("A leerer vorschlag bringt kam wos")
         timestamp = message.timestamp.strftime('%Y-%m-%d %H:%M')
         avatar = author.avatar_url if author.avatar \
             else author.default_avatar_url
