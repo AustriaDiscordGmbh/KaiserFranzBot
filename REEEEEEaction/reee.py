bc1a8e7d6f9dff5b69346270756ee3da3309e2de
David Bidner <david.bidner@student.tugraz.at>
2017-07-05 14:50:19 +0200

    ADD: info to reeeact

--- REEEEEEaction/reee.py
+++ REEEEEEaction/reee.py
@@ -3,16 +3,10 @@ import asyncio  # noqa: F401
 import discord
 import logging
 from discord.ext import commands
-from cogs.utils.dataIO import dataIO
 from cogs.utils import checks
 
 
 class ReeeAction:
-    """custom cog for a configureable suggestion box"""
-
-    __author__ = "sigttou"
-    __version__ = "0.0.1"
-
     def __init__(self, bot):
         self.bot = bot
         self.channel = "#shitpostingaeiou"
@@ -24,5 +18,4 @@ class ReeeAction:
             self.bot.say(self.channel)
 
 def setup(bot):
-    n = ReeeAction(bot)
-    bot.add_cog(n)
+    bot.add_cog(ReeeAction(bot))
