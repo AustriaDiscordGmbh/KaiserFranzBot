a2d241822ee2f384c72a6d23b3f311bc5ace5b4f
David Bidner <david.bidner@student.tugraz.at>
2017-07-28 14:51:42 +0200

    ADD: random beer post

---
+++ randbeer/randbeer.py
@@ -0,0 +1,24 @@
+import os
+import asyncio  # noqa: F401
+import discord
+import json
+import random
+import urllib2
+
+
+class RandBeer:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="beer", pass_context=True)
+    async def post_beer(self, ctx):
+        message = ctx.message
+        f = urllib2.urlopen('http://ajax.googleapis.com/ajax/services/search/images?q=beer&v=1.0&rsz=large&start=1')
+        data = json.load(f)
+        f.close()
+        results = data['responseData']['results']
+        url = results[random.randint(0, len(results) - 1)]['url']
+        await self.bot.send_message(message.channel, url)
+
+def setup(bot):
+    bot.add_cog(ReeeAction(bot))
