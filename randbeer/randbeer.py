e3182186d47a62771e0c7af8008e4bc92dae33fd
David Bidner <david.bidner@student.tugraz.at>
2017-07-28 14:59:09 +0200

    FIX: python3 urlopen

--- randbeer/randbeer.py
+++ randbeer/randbeer.py
@@ -3,7 +3,7 @@ import asyncio  # noqa: F401
 import discord
 import json
 import random
-import urllib
+from urllib.request import urlopen
 from discord.ext import commands
 
 
@@ -14,7 +14,7 @@ class RandBeer:
     @commands.command(name="beer", pass_context=True)
     async def post_beer(self, ctx):
         message = ctx.message
-        f = urllib.urlopen('http://ajax.googleapis.com/ajax/services/search/images?q=beer&v=1.0&rsz=large&start=1')
+        f = urlopen('http://ajax.googleapis.com/ajax/services/search/images?q=beer&v=1.0&rsz=large&start=1')
         data = json.load(f)
         f.close()
         results = data['responseData']['results']
